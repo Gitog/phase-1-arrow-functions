417b60c1c2f284c48d8da6d0469cedecd76f7bef
James <jamesnjorio@gmail.com>
2022-08-22 12:32:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (num1 = 2000, num2 = 100) {
+    let division = num1 / num2
+    return division;
+}
+// divide(2000, 100)
+
+const square = dimension => dimension * 2;
+square(2)
+
+const add = (a, b) => a + b;
+add(3, 4)
